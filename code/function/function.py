c0f018703a00391a329efe86653a3dc956c61028
rockslee <rockslee@sina.com>
2020-07-15 23:42:27 +0800

    删除杂乱代码，新增函数导入代码

---
+++ code/function/function.py
@@ -0,0 +1,19 @@
+def make_pizza(size,*toppings):
+    """
+    概述要制作的比萨
+    :param size:
+    :param topping:
+    :return:
+    """
+    print('\nMaking a '+str(size)+'-inch pizza with the following topping:')
+    for topping in toppings:
+        print('-'+topping)
+
+def bulid_profile(first,last,**user_info):
+    """创建一个字典，其中包括我们知道的用户的一切"""
+    profile = {}
+    profile[first]=first
+    profile[last]=last
+    for key,value in user_info.items():
+        profile[key]=value
+    return profile
---
+++ code/main.py
@@ -0,0 +1,5 @@
+from function.function import  make_pizza,bulid_profile as bp
+make_pizza(16,'pepperoni')
+make_pizza(12,'mushrooms','green peppers','extra cheese')
+user_profile=bp('rocks','lee',city='beijing',where='hebei')
+print(user_profile)
