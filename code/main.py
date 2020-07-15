b48bb815386a446158186cd39879843bb4c8e7e4
rockslee <rockslee@sina.com>
2020-07-16 01:34:30 +0800

    增加类学习。并且遇到class不能作为文件名和目录名，否则无法导入

--- code/main.py
+++ code/main.py
@@ -1,5 +1,13 @@
 from function.function import  make_pizza,bulid_profile as bp
+from classes.classes import Dog
 make_pizza(16,'pepperoni')
 make_pizza(12,'mushrooms','green peppers','extra cheese')
 user_profile=bp('rocks','lee',city='beijing',where='hebei')
 print(user_profile)
+
+my_dog = Dog('black',3)
+
+print("My dog's name is "+my_dog.name.title()+'.')
+print("My dog is "+str(my_dog.age)+' year old.')
+my_dog.roll_over()
+my_dog.sit()
