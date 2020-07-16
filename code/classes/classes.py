d93ebcdc3d2cb10fba3f197e255311d4a2f20fb8
rockslee <rockslee@sina.com>
2020-07-17 00:19:20 +0800

    新增： 1、重写父类方法完成； 2、将实例用作属性还没懂，要继续；

--- code/classes/classes.py
+++ code/classes/classes.py
@@ -56,3 +56,9 @@ class ElectricCar(Car):
     def describe_battery(self):
         """打印一条描述电瓶容量的消息"""
         print("This car has a "+str(self.battery_size)+"-KWh battery.")
+    def fill_gas_tank(self):
+        """电动汽车没有邮箱"""
+        print("This car doesn't need a gas tank!")
+class Battery():
+    """一次模拟电动汽车电瓶的简易尝试"""
+    def __init__(self,battery_size=70):
