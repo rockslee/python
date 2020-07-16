3dc1cb59df0a1462c53f501733eb3e9304b9650d
RocksLee <rockslee@sina.com>
2020-07-16 11:32:04 +0800

    新增： 1、新建car类； 2、练习修改实例属性值； 3、设置属性默认值； 4、未实例属性值修改增加条件；

--- code/main.py
+++ code/main.py
@@ -1,5 +1,5 @@
 from function.function import  make_pizza,bulid_profile as bp
-from classes.classes import Dog
+from classes.classes import Dog,Car
 make_pizza(16,'pepperoni')
 make_pizza(12,'mushrooms','green peppers','extra cheese')
 user_profile=bp('rocks','lee',city='beijing',where='hebei')
@@ -17,3 +17,12 @@ print("Your dog's name is "+your_dog.name.title()+\
       '.'+" It's "+str(your_dog.age)+' years old.')
 your_dog.sit()
 your_dog.roll_over()
+
+my_new_car = Car('audi','a4',2016)
+print(my_new_car.get_descriptive_name())
+print(my_new_car.model)
+my_new_car.read_odometer()
+my_new_car.update_odometer(100)
+my_new_car.read_odometer()
+my_new_car.update_odometer(99)
+my_new_car.read_odometer()
