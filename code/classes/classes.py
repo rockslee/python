c69b0dae7816334c3c388f1317bf2100467c5553
rockslee@sina.com <rockslee@sina.com>
2020-07-17 09:56:29 +0800

    新增： 1、将实例用作属性完成； 2、丰富实例的方法；

--- code/classes/classes.py
+++ code/classes/classes.py
@@ -44,6 +44,23 @@ class Car():
             self.odometer_reading += miles
         else:
             print("Your can't roll back an odometer!")
+class Battery():
+    """一次模拟电动汽车电瓶的简易尝试"""
+    def __init__(self,battery_size=70):
+        """初始化电瓶的属性"""
+        self.battery_size = battery_size
+    def describe_battery(self):
+        """打印一条描述电瓶容量的参数"""
+        print("This car has a "+str(self.battery_size)+"-KWh battery.")
+    def get_range(self):
+        """打印一条消息，指出电瓶的续航里程"""
+        if self.battery_size == 70:
+            range = 240
+        elif self.battery_size == 85:
+            range = 270
+        message = "This car can go approximately "+str(range)
+        message += " miles on a full charge"
+        print(message)
 class ElectricCar(Car):
     """电动汽车的独特之处"""
     def __init__(self,make,model,year):
@@ -52,13 +69,7 @@ class ElectricCar(Car):
         再初始化电动汽车特有的属性
         """
         super().__init__(make,model,year)
-        self.battery_size = 0
-    def describe_battery(self):
-        """打印一条描述电瓶容量的消息"""
-        print("This car has a "+str(self.battery_size)+"-KWh battery.")
+        self.battery = Battery()
     def fill_gas_tank(self):
         """电动汽车没有邮箱"""
         print("This car doesn't need a gas tank!")
-class Battery():
-    """一次模拟电动汽车电瓶的简易尝试"""
-    def __init__(self,battery_size=70):
