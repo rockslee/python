17c06d64635cbec2560a575c144da81eda9bebb2
rockslee@sina.com <rockslee@sina.com>
2020-07-19 10:03:42 +0800

    新增： 1、删除完成练习的代码； 2、按照9.3.6模拟实物要求，优化公里数类；

--- code/classes/classes.py
+++ code/classes/classes.py
@@ -1,41 +1,29 @@
-class  Dog():
-    """一次模拟小狗的简单尝试"""
-    def __init__(self,name,age):
-        """初始化属性name和age"""
-        self.name=name
-        self.age=age
-    def sit(self):
-        """模拟小狗被命令时蹲下"""
-        print(self.name.title()+' is now sitting.')
-    def roll_over(self):
-        """模拟小狗被命令时打滚"""
-        print(self.name.title() + ' rolled over.')
 class Odometer():
-    def __init__(self,energy=55):
+    """
+    支持电动汽车，燃油汽车里程计算
+    电动汽车类型为：1，燃油汽车类型为：0
+    """
+    def __init__(self,type=1,energy=55):
+        self.type = type
         self.energy = energy
     def get_range(self):
-        if self.energy == 55:
-            range = 100
-        elif self.energy == 75:
-            range = 120
+        if self.type == 0:
+            if self.energy == 55:
+                range = 100
+            elif self.energy == 75:
+                range = 120
+        elif self.type == 1:
+            if self.energy == 55:
+                range = 250
+            elif self.energy == 75:
+                range = 400
         message = "This car can go approximately " + str(range)
         message += " miles on a full charge"
         print(message)
-class Car():
-    """一次模拟汽车的简单尝试"""
-    def __init__(self,make,model,year):
-        """初始化描述汽车的属性"""
-        self.make = make
-        self.model = model
-        self.year = year
-        self.odometer_reading = 0
-    def get_descriptive_name(self):
-        """返回整洁的描述性信息"""
-        long_name = str(self.year)+' '+self.make+' '+self.model
-        return long_name.title()
-    def read_odometer(self):
-        """打印一条指出汽车里程的消息"""
-        print("This car has "+str(self.odometer_reading)+' miles on it.')
+    def modify_energy(self,energy):
+        self.energy = energy
+    def modify_type(self,type):
+        self.type = type
     def update_odometer(self,mileage):
         """
         将里程表读数设置未指定的值
@@ -63,6 +51,18 @@ class Battery():
     def describe_battery(self):
         """打印一条描述电瓶容量的参数"""
         print("This car has a "+str(self.battery_size)+"-KWh battery.")
+class Car():
+    """一次模拟汽车的简单尝试"""
+    def __init__(self,make,model,year):
+        """初始化描述汽车的属性"""
+        self.make = make
+        self.model = model
+        self.year = year
+        self.odometer_reading = Odometer()
+    def get_descriptive_name(self):
+        """返回整洁的描述性信息"""
+        long_name = str(self.year)+' '+self.make+' '+self.model
+        return long_name.title()
 class ElectricCar(Car):
     """电动汽车的独特之处"""
     def __init__(self,make,model,year):
--- code/main.py
+++ code/main.py
@@ -1,39 +1,10 @@
-from function.function import  make_pizza,bulid_profile as bp
-from classes.classes import Dog,Car,ElectricCar
-make_pizza(16,'pepperoni')
-make_pizza(12,'mushrooms','green peppers','extra cheese')
-user_profile=bp('rocks','lee',city='beijing',where='hebei')
-print(user_profile)
-
-my_dog = Dog('black',3)
-
-print("My dog's name is "+my_dog.name.title()+'.')
-print("My dog is "+str(my_dog.age)+' year old.')
-my_dog.roll_over()
-my_dog.sit()
-
-your_dog = Dog('flower',6)
-print("Your dog's name is "+your_dog.name.title()+\
-      '.'+" It's "+str(your_dog.age)+' years old.')
-your_dog.sit()
-your_dog.roll_over()
+from classes.classes import Car,ElectricCar
 
 my_new_car = Car('audi','a4',2016)
 print(my_new_car.get_descriptive_name())
-print(my_new_car.model)
-my_new_car.read_odometer()
-my_new_car.update_odometer(100)
-my_new_car.read_odometer()
-my_new_car.update_odometer(99)
-my_new_car.read_odometer()
-my_new_car.odometer_reading = 101
-my_new_car.read_odometer()
-my_new_car.increment_odometer(-10)
-my_new_car.read_odometer()
 e_car = ElectricCar('tesla','model s','2020')
 print(e_car.get_descriptive_name())
-e_car.read_odometer()
-e_car.update_odometer(200)
-e_car.read_odometer()
-e_car.battery.describe_battery()
+e_car.battery.range.modify_type(0)
+e_car.battery.range.modify_energy(75)
 e_car.battery.range.get_range()
+my_new_car.odometer_reading.get_range()
