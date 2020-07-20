ad0f1157305b63cd91a495a9ec241a46e97767b6
RocksLee <rockslee@sina.com>
2020-07-20 19:46:12 +0800

    新增： 1、linux环境下路径写法；

--- code/main.py
+++ code/main.py
@@ -1,7 +1,7 @@
-with open('file\love.txt','a') as file_object:
+with open('./file/love.txt','a') as file_object:
     file_object.write("I love you.\n")
     file_object.write("biu biu biu.\n")
     file_object.write("123456789\n")
-with open('file\love.txt') as file:
+with open('./file/love.txt') as file:
     contents = file.read()
     print(contents)
