05db0e2735b0dbbdcd20723eebc6e7817373ef5d
rockslee@sina.com <rockslee@sina.com>
2020-07-21 14:09:24 +0800

    新增： 1、练习json文件读取写入；

--- code/main.py
+++ code/main.py
@@ -1,17 +1,6 @@
-
-def count_words(filename):
-    try:
-        with open(filename) as f_obj:
-            contents = f_obj.read()
-    except FileNotFoundError:
-        msg = "Sorry,the file "+filename+"does not exist."
-        print(msg)
-    else:
-        # 计算文件大致包含多少个单词
-        words = contents.split()
-        num_words = len(words)
-        print("The file "+filename+" has about "+str(num_words)+" words.")
-
-filenames = ['./file/alice.txt','./file/love.txt','aa.txt']
-for filename in filenames:
-    count_words(filename)
+import json
+username = input("What is your name?")
+filename = './data/username.json'
+with open(filename,'w') as f_obj:
+    json.dump(username,f_obj)
+    print("We'll remeber you when you come back, "+username+'！')
